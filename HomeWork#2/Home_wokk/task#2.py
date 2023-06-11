2de77a7ceec0fc3e317a1aa19fa4a69f82798c4f
DIS686 <maksimdis31@gmail.com>
2023-06-11 20:29:23 +0300

    gg1

--- HomeWork#2/Home_wokk/task#2.py
+++ HomeWork#2/Home_wokk/task#2.py
@@ -14,17 +14,21 @@ from fractions import Fraction
 
 # Ввод дробей
 fraction1 = input("Введите первую дробь: ")
-fraction2 = input("Введите вторую дробь: ")
+per = list(fraction1)
+fraction_one = per[0]
+fraction_two = per[-1]
 
-a, b = map(int, input('Введите дроби через пробел').split())
+# fraction2 = input("Введите вторую дробь: ")
+
+# a, b = map(int, input('Введите дроби через пробел').split())
 
 
-def convert_fraction(fraction_one, fraction_two):
-    one = Fraction(fraction_one)
-    tow = Fraction(fraction_two)
+def convert_fraction(fraction_one_arg, fraction_two_arg):
+    one = Fraction(fraction_one_arg)
+    tow = Fraction(fraction_two_arg)
     sum_fraction = one + tow
     multiply_fraction = one * tow
     return f'Сумма дробей: {sum_fraction}\n Произведение дробей: {multiply_fraction}'
 
 
-convert_fraction(a, b)
+print(convert_fraction(int(fraction_one),int(fraction_two)))
