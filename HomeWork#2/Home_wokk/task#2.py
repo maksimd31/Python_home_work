672e903fd44a4da5a88b7806be5bc16534ab1b4c
M.B <imacon@iMac-MB.local>
2023-06-11 13:44:51 +0300

    Все что успел

--- HomeWork#2/Home_wokk/task#2.py
+++ HomeWork#2/Home_wokk/task#2.py
@@ -3,3 +3,28 @@
 # Программа должна возвращать сумму
 # и *произведение дробей. Для проверки своего
 # кода используйте модуль fractions.
+
+# Напишите программу, которая принимает две строки вида
+# “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль
+# fractions.
+
+
+from fractions import Fraction
+
+# Ввод дробей
+fraction1 = input("Введите первую дробь: ")
+fraction2 = input("Введите вторую дробь: ")
+
+a, b = map(int, input('Введите дроби через пробел').split())
+
+
+def convert_fraction(fraction_one, fraction_two):
+    one = Fraction(fraction_one)
+    tow = Fraction(fraction_two)
+    sum_fraction = one + tow
+    multiply_fraction = one * tow
+    return f'Сумма дробей: {sum_fraction}\n Произведение дробей: {multiply_fraction}'
+
+
+convert_fraction(a, b)
